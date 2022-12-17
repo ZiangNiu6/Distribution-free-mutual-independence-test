dc54fdc59db4fc331a6497807fbfbba8a6d8759c
Ziang Niu <79642702+ZiangNiu6@users.noreply.github.com>
2022-12-17 13:40:03 -0500

    Update Test_function.R

--- Test_function.R
+++ Test_function.R
@@ -176,6 +176,81 @@ boot_sample <- function(X, n, d, group){
   resample_X
 }
 
+## pairwise-independence 
+gensamdistrpwdcov <- function(N, dim_list, d = length(dim_list), niter=15000,
+                              fixgrid = halton(N, sum(dim_list))){
+  # create the combination
+  list_combn <- combn(1:length(dim_list), 2)
+  fixgrid_list <- list()
+  for (k in 1:d) {
+    if(k==1){
+      fixgrid_list[[k]] <- as.matrix(fixgrid[, (1:dim_list[k])])
+    }else{
+      fixgrid_list[[k]] <- as.matrix(fixgrid[, ((sum(dim_list[1:k-1])+1):(sum(dim_list[1:k])))])  
+    }
+  }
+  tstat = numeric(niter)
+  for(i in 1:niter)
+  {
+    per_list <- list()
+    per_list[[1]] <- as.matrix(fixgrid_list[[1]])
+    for (k in (1:(d-1))) {
+      per_list[[k+1]] <- as.matrix(fixgrid_list[[k+1]][sample(N), ])
+    }
+    for (m in 1:nrow(list_combn)) {
+      tstat[i] <- tstat[i] + energy::dcov(per_list[[list_combn[1,m]]], 
+                                          per_list[[list_combn[2,m]]], index = 2)
+    }
+  }
+  return(tstat)
+}
 
+computestatisticpwrdcov <- function(X, dim_list, n = nrow(X), d = length(dim_list),
+                                  gridch = halton(n, sum(dim_list)))
+{
+  data_list <- list()
+  X <- as.matrix(X)
+  gridch <- as.matrix(gridch)
+  for (k in 1:d) {
+    distmat <- matrix(0, nrow = n, ncol = n)
+    if (k == 1){
+      for(i in 1:n){
+        distmat[i,] = apply((X[i, (1:dim_list[k])]-t(gridch[, (1:dim_list[k])])), 2, Norm)^2
+      }
+      assignmentFUN <- solve_LSAP(distmat)
+      assignmentSOL <- cbind(seq_along(assignmentFUN), assignmentFUN)
+      data_list[[k]] <- as.matrix(gridch[assignmentSOL[,2], (1:dim_list[k])])
+    }else{
+      for(i in 1:n){
+        distmat[i,] = apply((X[i, ((sum(dim_list[1:k-1])+1):(sum(dim_list[1:k])))]
+                             -t(gridch[, ((sum(dim_list[1:k-1])+1):(sum(dim_list[1:k])))])), 2, Norm)^2
+      }
+      assignmentFUN <- solve_LSAP(distmat)
+      assignmentSOL <- cbind(seq_along(assignmentFUN), assignmentFUN)
+      data_list[[k]] <- as.matrix(gridch[assignmentSOL[,2], ((sum(dim_list[1:k-1])+1):(sum(dim_list[1:k])))])
+    }
+  }
+  randcovSTAT = pwdov(data_list, dim_list = dim_list)
+  return(randcovSTAT)
+}
 
+pwdov <- function(X, dim_list){
+  # create the combination
+  list_combn <- combn(1:length(dim_list), 2)
+  tstat = 0
+  for (m in 1:nrow(list_combn)) {
+    tstat <- tstat + energy::dcov(X[[list_combn[1,m]]], 
+                                  X[[list_combn[2,m]]], index = 2)
+  }
+  tstat
+}
 
+boot_pwdcov <- function(X, B, dim_list){
+  test_stat <- pwdov(X, dim_list = dim_list)
+  boot <- numeric(B)
+  n <- nrow(X[[1]])
+  for (b in 1:B) {
+    boot[b] <- pwdov(boot_sample_list(X, n, d = length(X)), dim_list = dim_list)
+  }
+  length(which(boot >= test_stat)) / (B + 1)
+}
